3c36123855716b578acb33aa12b2c1471d9298da
Jim Lawton <jim.lawton@gmail.com>
2022-05-26 17:18:01 +0100

    Fix fast-syntax-highlighting repo (original was deleted).

--- _setup_zsh.sh
+++ _setup_zsh.sh
@@ -5,7 +5,7 @@ if [ -d oh-my-zsh ]; then
         (cd oh-my-zsh/custom; ln -sf ../../override_window_title.zsh)
     fi
     if [ ! -e oh-my-zsh/custom/plugins/fast-syntax-highlighting ]; then
-        (cd oh-my-zsh/custom/plugins; git clone https://github.com/zdharma/fast-syntax-highlighting.git)
+        (cd oh-my-zsh/custom/plugins; git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git)
     fi
     echo "Done!"
 fi
