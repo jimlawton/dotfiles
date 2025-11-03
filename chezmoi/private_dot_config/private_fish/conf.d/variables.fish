9fbd91cd7d872df2c61f5a0caf5de84b78c96a92
Jim Lawton <jim.lawton@gmail.com>
2025-11-03 20:52:40 +0000

    fish: add variables.

---
+++ chezmoi/private_dot_config/private_fish/conf.d/variables.fish
@@ -0,0 +1,34 @@
+export OS=$(uname -s)
+
+#########################################################################
+# Tool settings
+#########################################################################
+
+# Set the default pager to less
+set -gx PAGER less
+# Set less to have a default flag of -R (RAW) so color passes through
+set pgx LESS "-R"
+
+set -gx HOSTNAME `hostname -s`
+set -gx EDITOR vim
+set -gx TMOUT 0
+
+set -gx PYTHONSTARTUP $HOME/.pythonrc
+set -gx WORKON_HOME $HOME/.virtualenvs
+set -gx PIP_VIRTUALENV_BASE $WORKON_HOME
+set -gx PIP_RESPECT_VIRTUALENV true
+set -gx PIP_REQUIRE_VIRTUALENV true
+
+set -gx PYENV_ROOT $HOME/.pyenv
+
+set -gx GOPATH $HOME/go
+set -gx GOENV_ROOT $HOME/.goenv
+
+set -gx TSRC_PARALLEL_JOBS 8
+
+if test -f $HOME/dotfiles/shellvars-personal
+    source $HOME/dotfiles/shellvars-personal
+fi
+if test -f $HOME/dotfiles/shellvars-work
+    source $HOME/dotfiles/shellvars-work
+fi
