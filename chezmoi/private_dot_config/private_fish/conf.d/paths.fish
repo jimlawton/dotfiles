d71266682dd9d58eed3b2b6b5e6f9659c6e30dbb
Jim Lawton <jim.lawton@gmail.com>
2025-11-03 22:14:09 +0000

    fish: update paths module.

--- chezmoi/private_dot_config/private_fish/conf.d/paths.fish
+++ chezmoi/private_dot_config/private_fish/conf.d/paths.fish
@@ -1,13 +1,11 @@
-fish_add_path .
-fish_add_path $HOME/bin
-fish_add_path $HOME/dotfiles/scripts
-fish_add_path $HOME/.cargo/bin
+fish_add_path --global --move --path "/opt/homebrew/bin" "/opt/homebrew/sbin"
 fish_add_path $GOPATH/bin
 fish_add_path $GOROOT/bin
-fish_add_path $HOME/adr-tools/src
 fish_add_path /usr/local/sbin
-fish_add_path /opt/homebrew/sbin
-fish_add_path /opt/homebrew/bin
 fish_add_path /usr/local/opt/gnu-sed/libexec/gnubin
 fish_add_path $GOENV_ROOT/bin
 fish_add_path /opt/homebrew/opt/llvm/bin
+fish_add_path $HOME/bin
+fish_add_path $HOME/dotfiles/scripts
+fish_add_path $HOME/.cargo/bin
+fish_add_path .
