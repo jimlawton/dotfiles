c06b7375a00e4546928a665cc5156b0ba3a5ccaf
Jim Lawton <jim.lawton@gmail.com>
2025-11-04 17:19:06 +0000

    Disable now abbrev.

--- chezmoi/private_dot_config/private_fish/conf.d/abbreviations.fish
+++ chezmoi/private_dot_config/private_fish/conf.d/abbreviations.fish
@@ -19,7 +19,7 @@ abbr --add fgrep 'fgrep --color=auto --exclude=.git'
 # abbr --add cp 'cp -i'
 # abbr --add mv 'mv -i'
 
-abbr --add now 'date +"%Y%m%d-%H%M%S"'
+# abbr --add now 'date +"%Y%m%d-%H%M%S"'
 abbr --add minicom 'minicom -C minicom-`now`.log'
 abbr --add pgrep 'ps auxww | grep \!* | grep -v grep'
 abbr --add a2ps 'a2ps --landscape -1 --line-numbers=5 --pretty-print --highlight-level=heavy --chars-per-line=120 --sides=duplex --prologue=color'
