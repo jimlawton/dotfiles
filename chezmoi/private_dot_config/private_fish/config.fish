75f943c16ddedb435d4452d6c81b7906c0dcc02a
Jim Lawton <jim.lawton@gmail.com>
2025-11-07 17:10:48 +0000

    Fix starship transient prompt.

--- chezmoi/private_dot_config/private_fish/config.fish
+++ chezmoi/private_dot_config/private_fish/config.fish
@@ -9,12 +9,9 @@ if status is-interactive
     # Don't be so friendly.
     set fish_greeting
 
-    # Starship prompt setup.
-    function starship_transient_prompt_func
-        starship module character
-    end
+    # Starship transient prompt setup.
     function starship_transient_rprompt_func
-        starship module time
+        starship module character
     end
     starship init fish | source
     enable_transience
