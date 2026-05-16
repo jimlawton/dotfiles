160608535e207b337e9130fa6a09787fd7506a80
Jim Lawton <jim.lawton@gmail.com>
2026-05-16 22:51:44 +0100

    chezmoi: reorder paths

--- chezmoi/private_dot_config/private_fish/conf.d/paths.fish
+++ chezmoi/private_dot_config/private_fish/conf.d/paths.fish
@@ -1,7 +1,7 @@
-fish_add_path --global --move --path $HOME/bin $HOME/.cargo/bin $GOPATH/bin
-fish_add_path --global --move --path $HOMEBREW_PREFIX/bin $HOMEBREW_PREFIX/sbin
-fish_add_path --global --move --path /usr/local/sbin
-fish_add_path --global --move --path /opt/homebrew/opt/gnu-sed/libexec/gnubin
-fish_add_path --global --move --path /opt/homebrew/opt/make/libexec/gnubin
-fish_add_path --global --move --path /opt/homebrew/opt/llvm/bin
-fish_add_path --global --move --path /Applications/Obsidian.app/Contents/MacOS
+fish_add_path --global --path $HOME/bin $HOME/.cargo/bin $GOPATH/bin
+fish_add_path --global --path $HOMEBREW_PREFIX/bin $HOMEBREW_PREFIX/sbin
+fish_add_path --global --path /usr/local/sbin
+fish_add_path --global --path /opt/homebrew/opt/gnu-sed/libexec/gnubin
+fish_add_path --global --path /opt/homebrew/opt/make/libexec/gnubin
+fish_add_path --global --path /opt/homebrew/opt/llvm/bin
+fish_add_path --global --path /Applications/Obsidian.app/Contents/MacOS
