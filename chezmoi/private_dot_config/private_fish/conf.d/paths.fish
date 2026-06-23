9acfeff83f6f0b5a7bdeb59a3780d9d9e31bb6ed
Jim Lawton <jim.lawton@gmail.com>
2026-06-23 19:50:44 +0100

    chezmoi: update fish paths (libpq)

--- chezmoi/private_dot_config/private_fish/conf.d/paths.fish
+++ chezmoi/private_dot_config/private_fish/conf.d/paths.fish
@@ -1,6 +1,7 @@
 fish_add_path --global --path $HOME/bin $HOME/.cargo/bin $HOME/go/bin
 fish_add_path --global --path $HOMEBREW_PREFIX/bin $HOMEBREW_PREFIX/sbin
 fish_add_path --global --path /usr/local/sbin
+fish_add_path --global --path /opt/homebrew/opt/libpq/bin
 fish_add_path --global --path /opt/homebrew/opt/gnu-sed/libexec/gnubin
 fish_add_path --global --path /opt/homebrew/opt/make/libexec/gnubin
 fish_add_path --global --path /opt/homebrew/opt/llvm/bin
