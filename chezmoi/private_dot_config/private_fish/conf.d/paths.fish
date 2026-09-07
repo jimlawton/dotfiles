3a3a751590d67a665b3dcfbd69eaec55fcf2bbf9
Jim Lawton <jim.lawton@gmail.com>
2026-09-07 11:03:40 +0100

    fish: add docker paths

--- chezmoi/private_dot_config/private_fish/conf.d/paths.fish
+++ chezmoi/private_dot_config/private_fish/conf.d/paths.fish
@@ -1,4 +1,4 @@
-fish_add_path --global --path $HOME/bin $HOME/.cargo/bin $HOME/go/bin
+fish_add_path --global --path $HOME/bin $HOME/.cargo/bin $HOME/go/bin $HOME/.docker/bin
 fish_add_path --global --path $HOMEBREW_PREFIX/bin $HOMEBREW_PREFIX/sbin
 fish_add_path --global --path /usr/local/sbin
 fish_add_path --global --path /opt/homebrew/opt/libpq/bin
@@ -6,3 +6,4 @@ fish_add_path --global --path /opt/homebrew/opt/gnu-sed/libexec/gnubin
 fish_add_path --global --path /opt/homebrew/opt/make/libexec/gnubin
 fish_add_path --global --path /opt/homebrew/opt/llvm/bin
 fish_add_path --global --path /Applications/Obsidian.app/Contents/MacOS
+
